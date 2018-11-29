ffa4a5ae87838ba27b5afdd7997395786a05df9f
chongmyeong <45442138+chongmyeong@users.noreply.github.com>
2018-11-29 14:14:40 +0900

    Add files via upload

---
+++ introudce.php
@@ -0,0 +1,81 @@
+<!DOCTYPE HTML>
+<html lang="ko">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no">
+<title>냥냥쓰 카페란?</title>
+<link rel="stylesheet" type="text/css" href="css/reset.css">
+<link rel="stylesheet" type="text/css" href="css/introduce.css">
+<link rel="shortcut icon" href="images/favicon/favicon.ico">
+<link rel="apple-touch-icon-precomposed" href="images/icon/aaa.png">
+<script src="js/jquery.min.js"></script>
+<script src="js/flat.min.js"></script>
+</head>
+<body>
+	<div id="wrap">
+		<section class="info_section">
+			<ul class="info_list">
+				<li><a href="index.html"><img src="images/s_images/info_icon_01.png" alt=""></a></li>
+				<li><a href="login.html"><img src="images/s_images/info_icon_02.png" alt=""></a></li>
+				<li><a href="password.html"><img src="images/s_images/info_icon_03.png" alt=""></a></li>
+				<li><a href=""><img src="images/s_images/info_icon_04.png" alt=""></a></li>
+			</ul>
+		</section>
+		<header class="header">
+			<h1 class="logo">
+				<a href="index.html">냥냥쓰<br>카페</a>
+			</h1>
+			<nav class="nav">
+				<ul class="gnb">
+					<li><a href="index.html">홈</a><span class="sub_menu_toggle_btn">하위 메뉴 토글 버튼</span></li>
+					<li><a href="introudce.html">냥냥쓰 카페 소개</a><span class="sub_menu_toggle_btn">하위 메뉴 토글 버튼</span></li>
+					<li><a href="gallery.html">메뉴</a><span class="sub_menu_toggle_btn">하위 메뉴 토글 버튼</span></li>
+					<li><a href="board.html">문의사항</a><span class="sub_menu_toggle_btn">하위 메뉴 토글 버튼</span></li>
+				</ul>
+			</nav>
+			<span class="menu_toggle_btn">전체 메뉴 토글 버튼</span>
+		</header>
+		<section class="sub_header_section">
+			<h2>Nyan Nyan's Cafe</h2>
+			<ul class="breadcrum_list">
+				<li><a href="index.html">홈 /</a></li>
+				<li><a href="introudce.html">냥냥쓰 카페란?</a></li>
+			</ul>
+		</section>
+        <section class="content_section">
+			<div class="content_row_1">
+				<img src="images/s_images/introduce_01.png" alt="">
+				<h3>냥냥쓰 카페</h3>
+				<p>냥냥쓰 카페는 건강한 맛을 추구합니다. 냥냥쓰의 레시피는 언제부터 시작되었는지는 알 수 없지만, 오래 전부터 전해져온 것은 분명합니다. 냥냥쓰는 매일 매일 맛있고 건강한 간식을 정성들여 만들어냅니다. 합성첨가물이 없으며, 동물복지 유정란으로 만든 맛있는 간식! 온라인 사이트를 만들어 달라는 고객님들의 성원에 드디어 나왔습니다!</p>
+			</div>
+			<div class="content_row_2">
+				<article>
+					<img src="images/s_images/introduce_02.jpg" alt="">
+					<h4>냥냥쓰 카페 쇼핑몰 이용 방법</h4>
+					<p>[메뉴]로 가서 물품을 주문 해 주세요! 그 다음 입금 해 주시면, 확인 완료 후 배송 해 드립니다.</p>
+				</article>
+				<article>
+					<img src="images/s_images/introduce_03.jpg" alt="">
+					<h4>냥냥쓰 과자를 더욱 맛잇게 먹기</h4>
+					<p>과자와 함께, 신메뉴인 [냥냥 밀크]와 함께 곁들여 드시면 더욱 좋습니다.</p>
+				</article>
+				<article>
+					<img src="images/s_images/introduce_04.jpg" alt="">
+					<h4>냥냥쓰에게 후원하는 방법</h4>
+					<p>냥협 000-0000-0000-00으로 현금을 쏘옥♥</p>
+				</article>	 
+			</div>
+			<div class="content_row_3">
+				<h4>냥냥쓰 카페의 미래</h4>
+				<div class="para">
+					<p>냥냥쓰 카페는 계속해서 신메뉴를 개발하고 있습니다. 할로윈 특선 메뉴인 [오싹오싹 랜덤박스]는 2시간 만에 조기 매진 되었고, 막대과자데이 특선 메뉴인 [냥냥 막대과자]는 1만여개를 주문받았습니다. 이번에는 [크리스마스 특대 랜덤박스]입니다. 많은 참여 부탁드립니다.</p>
+					<p>The Ranjint Cafe continues to develop new menus. The Halloween special, "Oak-Oak Random Box," was sold out early in two hours, and the bar-and-day special menu, "Gabbar" was ordered over 10,000. This is [Christmas Extra-Random Box]. We need your participation.</p>
+				</div>
+			</div>	
+		</section>
+		<footer class="footer">
+			<p>copyright&copy; 2018. nayn nayn's cafe ONLINE.</p>
+		</footer>
+	</div>
+</body>
+</html>
